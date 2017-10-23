6281e587c78420f8ca3a7021ef454a1561e017bf
SonicGD <sonicgd@gmail.com>
2017-10-23 12:21:26 +0500

    demo

---
+++ AppMetricsExcptionHandlerDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using App.Metrics.AspNetCore;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace AppMetricsExcptionHandlerDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .ConfigureMetricsWithDefaults((whBuilder, metricsBuilder) =>
+                {
+                    metricsBuilder.Configuration.Configure(options =>
+                    {
+                        options.DefaultContextLabel = "Test";
+                        options.GlobalTags["env"] = whBuilder.HostingEnvironment.IsStaging() ? "stage" :
+                            whBuilder.HostingEnvironment.IsProduction() ? "prod" : "dev";
+                    });
+                })
+                .UseMetrics()
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
